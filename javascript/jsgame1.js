444844d2a80b5806134fd1cfd8c9d4e586545de3
Everton Smith <evertonwsmith@gmail.com>
2024-05-22 20:32:18 -0700

    interactive element added (e)

--- javascript/jsgame1.js
+++ javascript/jsgame1.js
@@ -1,13 +1,60 @@
 //player creator
 var player;
-var obstacle1;
+var wall;
+var borderLeft;
+var borderRight;
+var borderTop;
+var borderBottom;
 var item1;
+var upAllowed = true;
+var downAllowed = true;
+var leftAllowed = true;
+var rightAllowed = true;
+var borders = [];
+var wallColor = 'rgb(37, 28, 135)';
+var tableColor = 'rgb(171, 135, 89)';
+var doorColor = 'rgb(74, 74, 74)';
+var interiorWallColor = 'black';
+var bookshelfColor = 'rgb(125, 72, 44)';
+var couchColor = 'rgb(117, 35, 35)';
+var available = [];
+var item1 = "Text Item";
 
 //Start function
 function startGame() {
     gameArea.start();
+    //800x600
     player = new component(30, 30, 'white', 400, 300);
-    obstacle1 = new component(30, 30, 'red', 500, 500);
+    wallLInterior = new wallComponent(20, 300, interiorWallColor, 150, 150);
+    wallRInterior = new wallComponent(20, 300, interiorWallColor, 650, 150);
+    wallTLInterior = new wallComponent(150, 20, interiorWallColor, 150, 150);
+    wallTRInterior = new wallComponent(200, 20, interiorWallColor, 450, 150);
+    wallBLInterior = new wallComponent(250, 20, interiorWallColor, 150, 450);
+    wallBRInterior = new wallComponent(120, 20, interiorWallColor, 550, 450);
+    table = new wallComponent(50,100,tableColor,550,250);
+    table2 = new wallComponent(30,80,tableColor,200,0);
+    table3 = new wallComponent(30,80,tableColor,200,200);
+
+    bookshelf1 = new interactiveComponent(25,150,bookshelfColor,125,200,[item1]);
+    bookshelf2 = new wallComponent(25,150,bookshelfColor,670,200);
+    bookshelf3 = new wallComponent(50,100,bookshelfColor,750,0);
+    bookshelf3 = new wallComponent(50,100,bookshelfColor,750,450);
+
+    borderTop = new wallComponent(800, 10, wallColor, 0, 0);
+    borderBottom = new wallComponent(800, 10, wallColor, 0, 590);
+    borderRight = new wallComponent(10, 600, wallColor, 790, 0);
+    borderLeft = new wallComponent(10, 600, wallColor, 0, 0);
+    couch = new wallComponent(150,50,couchColor,200,400);
+    comp1 = new wallComponent(25,25,tableColor,600,50);
+    comp2 = new wallComponent(25,25,tableColor,600,10);
+    comp3 = new wallComponent(25,25,tableColor,565,10);
+    comp4 = new wallComponent(25,25,tableColor,565,50);
+
+    door1 = new wallComponent(25,100,doorColor,0,150);
+    door2 = new wallComponent(25,100,doorColor,0,350);
+    door3 = new wallComponent(250,20,doorColor,250,0);
+    door4 = new wallComponent(250,20,doorColor,500,580);
+
 }
 
 document.body.style.width = '90vw';
@@ -16,6 +63,101 @@ document.body.style.margin = 'auto';
 document.body.style.display = 'flex';
 document.body.style.justifyContent = 'center';
 
+//Wall Component setup
+function wallComponent(width, height, color, x, y) {
+    this.width = width;
+    this.height = height;
+    this.backgroundColor = color;
+    this.x = x;
+    this.y = y;
+    borders.push(this);
+    this.update = function () {
+        ctx = gameArea.context;
+        ctx.fillStyle = color;
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+    this.isTouching = function (plyr) {
+        var leftWall = this.x;
+        var rightWall = this.x + (this.width);
+        var topWall = this.y;
+        var botWall = this.y + (this.height);
+        var plyrLeft = plyr.x;
+        var plyrRight = plyr.x + (plyr.width);
+        var plyrTop = plyr.y;
+        var plyrBot = plyr.y + (plyr.height);
+        var touching = true;
+        if (botWall >= plyrTop && topWall < plyrTop && leftWall < plyrRight && rightWall > plyrLeft) {
+            upAllowed = false;
+            plyr.speedY = Math.max(plyr.speedY, 0); // Stop only upward movement
+        }
+        if (topWall <= plyrBot && botWall > plyrBot && leftWall < plyrRight && rightWall > plyrLeft) {
+            downAllowed = false;
+            plyr.speedY = Math.min(plyr.speedY, 0); // Stop only downward movement
+        }
+        if (rightWall >= plyrLeft && leftWall < plyrLeft && topWall < plyrBot && botWall > plyrTop) {
+            leftAllowed = false;
+            plyr.speedX = Math.max(plyr.speedX, 0); // Stop only leftward movement
+        }
+        if (leftWall <= plyrRight && rightWall > plyrRight && topWall < plyrBot && botWall > plyrTop) {
+            rightAllowed = false;
+            plyr.speedX = Math.min(plyr.speedX, 0); // Stop only rightward movement
+        }
+        //add the rest of the logi
+        return touching;
+    }
+}
+
+
+//Wall Component setup
+function interactiveComponent(width, height, color, x, y, items) {
+    this.width = width;
+    this.height = height;
+    this.backgroundColor = color;
+    this.x = x;
+    this.y = y;
+    this.items = items;
+    borders.push(this);
+    this.update = function () {
+        ctx = gameArea.context;
+        ctx.fillStyle = color;
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+    this.isTouching = function (plyr) {
+        var leftWall = this.x;
+        var rightWall = this.x + (this.width);
+        var topWall = this.y;
+        var botWall = this.y + (this.height);
+        var plyrLeft = plyr.x;
+        var plyrRight = plyr.x + (plyr.width);
+        var plyrTop = plyr.y;
+        var plyrBot = plyr.y + (plyr.height);
+        var touching = true;
+        if (botWall >= plyrTop && topWall < plyrTop && leftWall < plyrRight && rightWall > plyrLeft) {
+           available = this.items;
+        }
+        else if (topWall <= plyrBot && botWall > plyrBot && leftWall < plyrRight && rightWall > plyrLeft) {
+            available = this.items;
+        }
+        else if (rightWall >= plyrLeft && leftWall < plyrLeft && topWall < plyrBot && botWall > plyrTop) {
+            available = this.items;
+        }
+        else if (leftWall <= plyrRight && rightWall > plyrRight && topWall < plyrBot && botWall > plyrTop) {
+            available = this.items;
+        }
+        else {
+            available=[];
+        }
+        //add the rest of the logi
+        return touching;
+    }
+}
+
+function action(){
+    for(let v of available){
+        console.log(v);
+    }
+
+}
 
 //Component setup
 function component(width, height, color, x, y) {
@@ -35,7 +177,6 @@ function component(width, height, color, x, y) {
         this.x += this.speedX;
         this.y += this.speedY;
     }
-
     this.crashWith = function (otherObject) {
         var myLeft = this.x;
         var myRight = this.x + (this.width);
@@ -55,16 +196,24 @@ function component(width, height, color, x, y) {
 
 //Motion Functions
 function up() {
-    player.speedY = -5;
+    if (upAllowed) {
+        player.speedY = -5;
+    }
 }
 function down() {
-    player.speedY = 5;
+    if (downAllowed) {
+        player.speedY = 5;
+    }
 }
 function left() {
-    player.speedX = -5;
+    if (leftAllowed) {
+        player.speedX = -5;
+    }
 }
 function right() {
-    player.speedX = 5;
+    if (rightAllowed) {
+        player.speedX = 5;
+    }
 }
 
 function stop() {
@@ -89,6 +238,7 @@ function handleKeyDown(event) {
             break;
         case 'e':
             //implement action button
+            action();
             break;
     }
 }
@@ -105,7 +255,7 @@ var gameArea = {
     start: function () {
         this.canvas.width = 800;
         this.canvas.height = 600;
-        this.canvas.style.backgroundColor = 'rgb(30,30,50)';
+        this.canvas.style.backgroundColor = 'rgb(178, 171, 245)';
         this.canvas.style.margin = 'auto';
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
@@ -122,13 +272,25 @@ var gameArea = {
 
 function updateGameArea() {
 
-    if (player.crashWith(obstacle1)) {
-        gameArea.stop();
-    }
-
     gameArea.clear();
-    obstacle1.update();
+    checkCollisions();
+
     player.newPos();
     player.update();
 }
 
+function checkCollisions() {
+    upAllowed = true;
+    downAllowed = true;
+    leftAllowed = true;
+    rightAllowed = true;
+
+    for (let bord of borders) {
+        bord.isTouching(player);
+    }
+
+    for (let bord of borders) {
+        bord.update(player);
+    }
+
+}
