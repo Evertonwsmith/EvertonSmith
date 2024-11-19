fb180f0e4f0db0e5be83dc2f615111ce13e883af
Everton Smith <evertonwsmith@gmail.com>
2024-11-18 20:18:11 -0800

    new page started

---
+++ Deprecated/lightdarktoggle/index.js
@@ -0,0 +1,10 @@
+//javascript check id theme-toggle for click to toggle .dark-mode css
+
+document.addEventListener('DOMContentLoaded', function() {
+    const themeToggle = document.getElementById('theme-toggle');
+    const body = document.body;
+
+    themeToggle.addEventListener('click', function() {
+        body.classList.toggle('dark-mode');
+    });
+});
